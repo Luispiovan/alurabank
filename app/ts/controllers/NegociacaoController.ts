7b11c6c43dd192118cfc39a5ebc109eb97bb58c4
Luis Guilherme <luis.piovan.lg@gmail.com>
2021-05-10 15:09:00 -0300

    Include decorators

--- app/ts/controllers/NegociacaoController.ts
+++ app/ts/controllers/NegociacaoController.ts
@@ -1,24 +1,26 @@
 import { NegociacoesView, MensagemView } from '../views/index';
 import { Negociacao, Negociacoes } from '../models/index';
+import { domInject } from '../helpers/decorators/domInject';
 
 export class NegociacaoController {
 
+    @domInject('#data')
     private _inputData: JQuery;
+
+    @domInject('#quantidade')
     private _inputQuantidade: JQuery;
+
+    @domInject('#valor')
     private _inputValor: JQuery;
     private _negociacoes = new Negociacoes();
     private _negociacoesView = new NegociacoesView('#negociacoesView');
     private _mensagemView = new MensagemView('#mensagemView');
     
     constructor() {
-        this._inputData = $('#data');
-        this._inputQuantidade = $('#quantidade');
-        this._inputValor = $('#valor');
         this._negociacoesView.update(this._negociacoes);
     }
 
     adiciona(event: Event): void {
-
         event.preventDefault();
 
         let data = new Date(this._inputData.val().toString().replace(/-/g, ','));
@@ -37,6 +39,7 @@ export class NegociacaoController {
 
         this._negociacoesView.update(this._negociacoes);
         this._mensagemView.update('Negociação adicionada com sucesso!');
+
     }
 
     private _ehDiaUtil(_data: Date): boolean {
@@ -44,7 +47,7 @@ export class NegociacaoController {
     }
 }
 
-enum DiaDaSemana {
+enum DiaDaSemana{
     Domingo,
     Segunda,
     Terca,
